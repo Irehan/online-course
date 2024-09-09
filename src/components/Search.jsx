3046fff5153f5ab350a9bfb8866917f6258de20b
Ali Rehan Haider <007rehan16@gmail.com>
2024-09-09 18:14:19 +0530

    Search Popup fixed issue

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,26 +1,41 @@
-export const Search = () => (
-    <div className="search-right">
-        <a href="#search" title="search">
-            <span className="fa fa-search" aria-hidden="true" />
-        </a>
-        <div id="search" className="pop-overlay">
-            <div className="popup">
-                <form action="error.html" method="GET" className="search-box">
-                    <input
-                        type="search"
-                        placeholder="Search"
-                        name="search"
-                        required="required"
-                        autofocus=""
-                    />
-                    <button type="submit" className="btn">
-                        <span className="fa fa-search" aria-hidden="true" />
-                    </button>
-                </form>
-            </div>
-            <a className="close" href="#close">
-                ×
-            </a>
+import { useState } from 'react';
+
+export const Search = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    // Toggle popup visibility
+    const togglePopup = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <div className="search-right">
+            {/* Instead of using href, use an onClick handler */}
+            <span className="fa fa-search" aria-hidden="true" onClick={togglePopup} />
+
+            {/* Conditionally render the popup */}
+            {isOpen && (
+                <div className="pop-overlay">
+                    <div className="popup">
+                        <form action="error.html" method="GET" className="search-box">
+                            <input
+                                type="search"
+                                placeholder="Search"
+                                name="search"
+                                required="required"
+                                autoFocus
+                            />
+                            <button type="submit" className="btn">
+                                <span className="fa fa-search" aria-hidden="true" />
+                            </button>
+                        </form>
+                        {/* Close button */}
+                        <span className="close" onClick={togglePopup}>
+                            ×
+                        </span>
+                    </div>
+                </div>
+            )}
         </div>
-    </div>
-)
+    );
+};
