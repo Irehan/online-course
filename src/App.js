9e7de6918737f89ba3d1cf6891b3515518d83a7c
Ali Rehan Haider <007rehan16@gmail.com>
2024-09-13 02:42:46 +0530

    minr-changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Courses } from "./pages/Courses";
 import { Home } from "./pages/Home";
 import { HashRouter, BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
+import { CoursesProvider } from "./context/CoursesContext"; // Import CoursesProvider
 
 // Conditionally setting AppRouter based on environment
 const AppRouter =
@@ -12,16 +13,18 @@ const AppRouter =
 
 function App() {
   return (
-    <AppRouter>
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/courses" element={<Courses />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </AppRouter>
+    <CoursesProvider>
+      <AppRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </AppRouter>
+    </CoursesProvider>
   );
 }
 
--- src/context/CoursesContext.js
+++ src/context/CoursesContext.js
@@ -1,18 +1,15 @@
 import React, { createContext, useState, useContext } from "react";
-import coursesData from "../data/coursesData";
+import coursesData from "../data/coursesData"; // Adjust path as needed
 
 const CoursesContext = createContext();
 
 export const CoursesProvider = ({ children }) => {
   const [courses, setCourses] = useState(coursesData);
 
-  const value = {
-    courses,
-    setCourses,
-  };
-
   return (
-    <CoursesContext.Provider value={value}>{children}</CoursesContext.Provider>
+    <CoursesContext.Provider value={{ courses, setCourses }}>
+      {children}
+    </CoursesContext.Provider>
   );
 };
 
