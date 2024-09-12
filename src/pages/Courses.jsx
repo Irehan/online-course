049c004db6ad7def75d6b03ff6c466fb87b1b454
Ali Rehan Haider <007rehan16@gmail.com>
2024-09-12 16:04:00 +0530

    Update-changes

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -3,7 +3,7 @@ import { Header } from '../components/Header'
 import { Footer } from '../components/Footer'
 import MoveToTop from '../components/MoveToTop';
 import Breadcrumb from '../components/Breadcrumb';
-import OurCourses from '../components/OurCourses';
+// import OurCourses from '../components/OurCourses';
 import Pagination from '../components/Pagination';
 import CoursesList from '../components/CoursesList';
 export const Courses = () => (
