be7ea5a91b59b83cdad933f0c1ce7b93ad199be7
Ali Rehan Haider <007rehan16@gmail.com>
2024-09-12 16:21:28 +0530

    redo-changes

--- src/components/CoursesList.jsx
+++ src/components/CoursesList.jsx
@@ -43,23 +43,29 @@ const courses = [
 
 const CoursesList = () => {
     return (
-        <div className="row">
-            {courses.map((course, index) => (
-                <OurCourses
-                    key={index}
-                    image={course.image}
-                    title={course.title}
-                    price={course.price}
-                    duration={course.duration}
-                    students={course.students}
-                    level={course.level}
-                    rating={course.rating}
-                    author={course.author}
-                    authorImg={course.authorImg}
-                    category={course.category}
-                />
-            ))}
-        </div>
+        <section className="w3l-courses">
+            <div className="blog pb-5" id="courses">
+                <div className="container py-lg-5 py-md-4 py-2">
+                    <div className="row">
+                        {courses.map((course, index) => (
+                            <OurCourses
+                                key={index}
+                                image={course.image}
+                                title={course.title}
+                                price={course.price}
+                                duration={course.duration}
+                                students={course.students}
+                                level={course.level}
+                                rating={course.rating}
+                                author={course.author}
+                                authorImg={course.authorImg}
+                                category={course.category}
+                            />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
--- src/components/OurCourses.jsx
+++ src/components/OurCourses.jsx
@@ -3,63 +3,61 @@ import React from 'react';
 
 function OurCourses({ image, title, price, duration, students, level, rating, author, authorImg, category }) {
     return (
-        <section className="w3l-courses">
-            <div className="blog pb-5" id="courses">
-                <div className="container py-lg-5 py-md-4 py-2">
-                    <div className="row">
-                        {/* Card 1 */}
-                        <div className="col-lg-4 col-md-6 item">
-                            <div className="card">
-                                <div className="card-header p-0 position-relative">
-                                    <a href="#course-single" className="zoom d-block">
-                                        <img className="card-img-bottom d-block" src={image} alt={title} />
-                                    </a>
-                                    <div className="post-pos">
-                                        <a href="#reciepe" className="receipe blue">{level}</a>
-                                    </div>
-                                </div>
-                                <div className="card-body course-details">
-                                    <div className="price-review d-flex justify-content-between mb-1 align-items-center">
-                                        <p>{price}</p>
-                                        <ul className="rating-star">
-                                            {Array(5).fill().map((_, i) => (
-                                                <li key={i}>
-                                                    <span className={`fa ${i < rating ? "fa-star" : "fa-star-o"}`}></span>
-                                                </li>
-                                            ))}
-                                        </ul>
-                                    </div>
-                                    <a href="#course-single" className="course-desc">{title}</a>
-                                    <div className="course-meta mt-4">
-                                        <div className="meta-item course-lesson">
-                                            <span className="fa fa-clock-o"></span>
-                                            <span className="meta-value"> {duration} hrs </span>
-                                        </div>
-                                        <div className="meta-item course-">
-                                            <span className="fa fa-user-o"></span>
-                                            <span className="meta-value"> {students} </span>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="card-footer">
-                                    <div className="author align-items-center">
-                                        <img src={authorImg} alt={author} className="img-fluid rounded-circle" />
-                                        <ul className="blog-meta">
-                                            <li>
-                                                <span className="meta-value mx-1">by</span> <a href="#author"> {author}</a>
-                                            </li>
-                                            <li>
-                                                <span className="meta-value mx-1">in</span> <a href="#category"> {category}</a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
+
+
+        <>
+            {/* Card 1 */}
+            <div className="col-lg-4 col-md-6 item">
+                <div className="card">
+                    <div className="card-header p-0 position-relative">
+                        <a href="#course-single" className="zoom d-block">
+                            <img className="card-img-bottom d-block" src={image} alt={title} />
+                        </a>
+                        <div className="post-pos">
+                            <a href="#reciepe" className="receipe blue">{level}</a>
+                        </div>
+                    </div>
+                    <div className="card-body course-details">
+                        <div className="price-review d-flex justify-content-between mb-1 align-items-center">
+                            <p>{price}</p>
+                            <ul className="rating-star">
+                                {Array(5).fill().map((_, i) => (
+                                    <li key={i}>
+                                        <span className={`fa ${i < rating ? "fa-star" : "fa-star-o"}`}></span>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                        <a href="#course-single" className="course-desc">{title}</a>
+                        <div className="course-meta mt-4">
+                            <div className="meta-item course-lesson">
+                                <span className="fa fa-clock-o"></span>
+                                <span className="meta-value"> {duration} hrs </span>
                             </div>
+                            <div className="meta-item course-">
+                                <span className="fa fa-user-o"></span>
+                                <span className="meta-value"> {students} </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card-footer">
+                        <div className="author align-items-center">
+                            <img src={authorImg} alt={author} className="img-fluid rounded-circle" />
+                            <ul className="blog-meta">
+                                <li>
+                                    <span className="meta-value mx-1">by</span> <a href="#author"> {author}</a>
+                                </li>
+                                <li>
+                                    <span className="meta-value mx-1">in</span> <a href="#category"> {category}</a>
+                                </li>
+                            </ul>
                         </div>
                     </div>
                 </div>
             </div>
-        </section>
+        </>
+
+
     );
 }
 
