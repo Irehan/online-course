417da46c354128f22c8295870f28a5e666200ac3
Ali Rehan Haider <007rehan16@gmail.com>
2024-09-13 01:25:35 +0530

    Course-page optimizing

--- src/components/CoursesList.jsx
+++ src/components/CoursesList.jsx
@@ -1,44 +1,7 @@
 import React from 'react';
 import CourseCard from './CourseCard';  // Updated import
+import courses from './coursesData'; // Adjust the path as necessary
 
-const courses = [
-    {
-        image: "assets/images/c1.jpg",
-        title: "Open Programming Courses for everyone: Python",
-        price: "$35.00",
-        duration: 20,
-        students: 50,
-        level: "Beginner",
-        rating: 4,
-        author: "Olivia",
-        authorImg: "assets/images/a1.jpg",
-        category: "Programming",
-    },
-    {
-        image: "assets/images/c2.jpg",
-        title: "Learn Master JQuery in a Short Period of Time",
-        price: "$49.00",
-        duration: 20,
-        students: 50,
-        author: "William",
-        category: "Programming",
-        level: "Intermediate",
-        rating: 5,
-        authorImg: "assets/images/a2.jpg",
-    },
-    {
-        image: "assets/images/c3.jpg",
-        title: "Learning to Write as a Clean and Professional Author",
-        price: "$0.00",
-        duration: 20,
-        students: 50,
-        author: "Isabella",
-        category: "Teaching",
-        level: "Beginner",
-        rating: 5,
-        authorImg: "assets/images/a3.jpg",
-    },
-];
 
 const CoursesList = () => {
     return (
---
+++ src/components/coursesData.js
@@ -0,0 +1,41 @@
+// coursesData.js
+const courses = [
+  {
+    image: "assets/images/c1.jpg",
+    title: "Open Programming Courses for everyone: Python",
+    price: "$35.00",
+    duration: 20,
+    students: 50,
+    level: "Beginner",
+    rating: 4,
+    author: "Olivia",
+    authorImg: "assets/images/a1.jpg",
+    category: "Programming",
+  },
+  {
+    image: "assets/images/c2.jpg",
+    title: "Learn Master JQuery in a Short Period of Time",
+    price: "$49.00",
+    duration: 20,
+    students: 50,
+    author: "William",
+    category: "Programming",
+    level: "Intermediate",
+    rating: 5,
+    authorImg: "assets/images/a2.jpg",
+  },
+  {
+    image: "assets/images/c3.jpg",
+    title: "Learning to Write as a Clean and Professional Author",
+    price: "$0.00",
+    duration: 20,
+    students: 50,
+    author: "Isabella",
+    category: "Teaching",
+    level: "Beginner",
+    rating: 5,
+    authorImg: "assets/images/a3.jpg",
+  },
+];
+
+export default courses;
