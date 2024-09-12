5f5022021ed1837d7e85606fe0c97b4ecd8f886b
Ali Rehan Haider <007rehan16@gmail.com>
2024-09-13 01:45:43 +0530

    Course-opt

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -9,8 +9,38 @@ export const Courses = () => (
     <>
         <Header />
         <Breadcrumb />
-        <CoursesList />
-        <Pagination />
+        <section className="w3l-courses">
+            <div className="blog pb-5" id="courses">
+                <div className="container py-lg-5 py-md-4 py-2">
+                    <div className="row">
+                        <CourseCard
+                            image={course1.image}
+                            title={course1.title}
+                            price={course1.price}
+                            duration={course1.duration}
+                            students={course1.students}
+                            level={course1.level}
+                            rating={course1.rating}
+                            author={course1.author}
+                            authorImg={course1.authorImg}
+                            category={course1.category}
+                        />
+                        <CourseCard
+                            image={course2.image}
+                            title={course2.title}
+                            price={course2.price}
+                            duration={course2.duration}
+                            students={course2.students}
+                            level={course2.level}
+                            rating={course2.rating}
+                            author={course2.author}
+                            authorImg={course2.authorImg}
+                            category={course2.category}
+                        />
+                    </div>
+                </div>
+            </div>
+        </section>
         <MoveToTop />
         <Footer />
     </>
